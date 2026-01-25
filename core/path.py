afacbddc49e5110416c5735cddef3fc22bf5467c
kkghrsbsb <395508188@qq.com>
2026-01-25 18:24:25 +0800

    refactor: 重构文件结构

---
+++ core/path.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def find_project_root(start: Path) -> Path:
+    for p in [start, *start.parents]:
+        if (p / ".git").exists():
+            return p
+    raise RuntimeError("Project root not found")
+
+# .git 为根目录
+ROOT = find_project_root(Path(__file__).resolve())
+
+ASSETS_DIR = ROOT / "assets"
+CONFIG_DIR = ROOT / "configs"
+
+ROBOTS_DIR = ASSETS_DIR / "robots"
+
+PIPER_DESCRIPTION_DIR = ROBOTS_DIR / "piper_description"
--- learn_bullet/run_gui.py
+++ learn_bullet/run_gui.py
@@ -2,15 +2,17 @@ import os
 import time
 import pybullet as p
 import pybullet_data
+from core.path import PIPER_DESCRIPTION_DIR
 
 p.connect(p.GUI, options="--width=1280 --height=720")
 p.setGravity(0, 0, -9.8)
 
+# 从 pybullet_data 获取环境
 plane_path = os.path.join(pybullet_data.getDataPath(), "plane.urdf")
 planeId = p.loadURDF(plane_path)
 
-ROOT = os.path.dirname(os.path.abspath(__file__))
-robot_path = os.path.join(ROOT, "piper_description", "urdf", "piper_description_v100_camera.urdf")
+# 
+robot_path = os.path.join(PIPER_DESCRIPTION_DIR, "urdf", "piper_description_v100_camera.urdf")
 robotId = p.loadURDF(robot_path, [0, 0, 0], useFixedBase=True, globalScaling=5)
 
 def blsleep(timer: float):
